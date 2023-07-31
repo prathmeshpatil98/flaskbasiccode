a405d222ab998d3801158f6e6ca6cb9cb04a86a8
prathmeshpatil98 <prathmesh8485049898@gmail.com>
2023-07-31 07:24:20 +0530

    my first code

---
+++ filereading.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd 
+import numpy as np
+
+
+# In[ ]:
+
+
+
+
+
+# In[4]:
+
+
+file = pd.read_csv("Transformed_Housing_Data2.csv")
+
+
+# In[5]:
+
+
+file
+
+
+# In[12]:
+
+
+file.head(10)
+
+
+# In[6]:
+
+
+file
+
